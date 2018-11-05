3a879ab4829185b76e48d20c73f280471e9e34ed
Josip Kovačević <38467885+jkovacevic992@users.noreply.github.com>
2018-11-05 16:42:24 +0100

    First commit

---
+++ MathGame/app/src/main/java/k/josip/mathgame/MainActivity.java
@@ -0,0 +1,70 @@
+package k.josip.mathgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView firstNum;
+    TextView secondNum;
+    TextView solution;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        fillNumbers();
+
+
+        Button btn = (Button) findViewById(R.id.submit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            
+            public void onClick(View v) {
+                TextView res = findViewById(R.id.result);
+
+
+                if(checkResult(Integer.parseInt(res.getText().toString()))){
+                    fillNumbers();
+                    res.setText("");
+                }
+            }
+        });
+
+    }
+    public int randomNum(){
+        int random = new Random().nextInt(100);
+        return random;
+    }
+
+
+    public void fillNumbers(){
+        firstNum = findViewById(R.id.firstNum);
+        secondNum = findViewById(R.id.secondNum);
+
+        firstNum.setText(String.valueOf(randomNum()));
+        secondNum.setText(String.valueOf(randomNum()));
+    }
+
+    public boolean checkResult(int num){
+        int first = Integer.parseInt(firstNum.getText().toString());
+        int second = Integer.parseInt((secondNum.getText().toString()));
+        solution = findViewById(R.id.txtSolution);
+        if(num==(first+second)){
+
+           solution.setText("Correct");
+           return true;
+        }else {
+            solution.setText("False");
+        }
+        return false;
+    }
+
+
+
+
+}
