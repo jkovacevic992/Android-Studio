928120f1a75d89b0f423962df77896bca888b609
Josip Kovačević <38467885+jkovacevic992@users.noreply.github.com>
2018-11-20 17:44:38 +0100

    Finished 14. Exercise: Detect Faces Intro

---
+++ AdvancedAndroid_Emojify/app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -0,0 +1,11 @@
+package com.example.android.emojify;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+public class Emojifier {
+
+    static void detectFaces(Context context, Bitmap bitmap){
+
+    }
+}
