c7028d2d2669f48a27120550e10e375caad0f5c2
Josip Kovačević <38467885+jkovacevic992@users.noreply.github.com>
2018-11-21 18:32:03 +0100

    Completed 17. Exercise: Get Probabilities

--- AdvancedAndroid_Emojify/app/src/main/java/com/example/android/emojify/Emojifier.java
+++ AdvancedAndroid_Emojify/app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -2,10 +2,46 @@ package com.example.android.emojify;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.util.Log;
+import android.util.SparseArray;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
 
 public class Emojifier {
+    static final String LOG_TAG="";
 
     static void detectFaces(Context context, Bitmap bitmap){
+        FaceDetector detector = new FaceDetector.Builder(context)
+                .setTrackingEnabled(false)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+
+        SparseArray<Face> faces = detector.detect(frame);
+
+        Log.d(LOG_TAG, "detectFaces: number of faces = " + faces.size());
+
+        if (faces.size()==0){
+            Toast.makeText(context,"No faces detected", Toast.LENGTH_SHORT).show();
+        }else{
+            for (int i=0; i<faces.size(); i++){
+                Face face = faces.valueAt(i);
+                getClassifications(face);
+
+            }
+        }
+        detector.release();
+    }
+
+    private static void getClassifications(Face face){
+
+        Log.d(LOG_TAG, "getClassifications: smilingProb = " + face.getIsSmilingProbability());
+        Log.d(LOG_TAG,"getClassifications: leftEyeOpenProb = " + face.getIsLeftEyeOpenProbability());
+        Log.d(LOG_TAG,"getClassifications: rightEyeOpenProb = " + face.getIsRightEyeOpenProbability());
 
     }
 }
